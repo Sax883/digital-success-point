1c89ed3d1c431f6bb16962de20956a5eec96201c
CyberNexus <first.last.dhl@gmailcom>
2026-08-04 12:13:55 -0700

    Fix page routes and navbar link targets

--- server.js
+++ server.js
@@ -2,11 +2,27 @@ require('dotenv').config();
 
 const path = require('path');
 const express = require('express');
-const app = require('./api');
+const apiApp = require('./api');
 const { connectToDatabase } = require('./db');
+
+const app = express();
 
 app.use(express.static(path.join(__dirname, '.')));
 app.use('/image', express.static(path.join(__dirname, 'image')));
+
+app.get('/login.html', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.get('/signup.html', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'signup.html'));
+});
+
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.use(apiApp);
 
 const port = process.env.PORT || 3000;
 
